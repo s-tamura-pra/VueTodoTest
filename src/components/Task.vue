4a7044e5c148d2bc9799af68597290d538f160e0
s-tamura <lohas.barista@gmail.com>
2018-11-07 18:38:55 +0900

    ログアウト機能追加

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,29 +1,29 @@
 //アプリのメインとなるページを作成(新規作成)
 <template>
-    <div id = "task">
-        <h2>ToDoページ</h2>
-        <div>
-            <input type="text" v-model="newTodoName">
-            <buton type="submit" v-on:click="createTodo()">新規作成</buton>
-        </div>
-        <ul>
-            <li><button type="submit" v-on:click="showTodoType" = 'all'>全て</button></li>
-            <li><button type="submit" v-on:click="showTodoType" = 'active'>対応中タスク一覧</button></li>
-            <li><button type="submit" v-on:click="showTodoType" = 'complete'>完了タスク一覧</button></li>            
-        </ul>
-
-        <ul v-for="(todo, key) in filteredTodos" :key="todo.id">
-            <li>
-                <input class="toggle" type="checkbox" v-model="todo.isComplete" v-on:click="updateIsCompleteTodo(todo, key)">{{ todo.name }}</li>
-            </li>
-            <button type="submit" v-on:click="deleteTodo(key)">削除</button>
-        </ul>
+  <div class="task">
+     <h2>タスク</h2>
+    <div>
+      <input type="text" v-model="newTodoName">
+      <button type="submit" v-on:click="createTodo()">タスク作成</button>
     </div>
+    <ul class="taskList">
+      <li><button type="submit" v-on:click="showTodoType = 'all'">すべて</button></li>
+      <li><button type="submit" v-on:click="showTodoType = 'active'">未完タスク一覧</button></li>
+      <li><button type="submit" v-on:click="showTodoType = 'complete'">完了タスク一覧</button></li>
+    </ul>
+    <!-- todoの一覧表示 -->
+    <ul v-for="(todo, key) in filteredTodos" :key="todo.id">
+      <li><input class="toggle" type="checkbox" v-model="todo.isComplete" v-on:click="updateIsCompleteTodo(todo, key)">{{ todo.name }}</li>
+      <button type="submit" v-on:click="deleteTodo(key)">削除</button>
+    </ul>
+
+    <button @click="signOut">サインアウト</button>
+  </div>
 </template>
 
 <script>
-    import firebase from "firebase";
-
+import firebase from "firebase";
+ 
 export default {
   name: "Task",
   data() {
@@ -39,7 +39,7 @@ export default {
     this.database = firebase.database();
     this.uid = firebase.auth().currentUser.uid;
     this.todosRef = this.database.ref("todos/" + this.uid);
-
+ 
     var _this = this;
     // データに変更があると実行されるfunction
     this.todosRef.on("value", (snapshot) => {
@@ -68,6 +68,12 @@ export default {
     }
   },
   methods: {
+    //signout
+    signOut: function() {
+      firebase.auth().signOut().then(() => {
+        this.$router.push('/signin')
+      })
+    },
     // DBのtodos/[uid]/以下にデータを格納していく
     createTodo: function() {
       if (this.newTodoName == "") {
@@ -95,4 +101,7 @@ export default {
 </script>
 
 <style>
+  .tasklist{
+    list-style:none;
+  }
 </style>
